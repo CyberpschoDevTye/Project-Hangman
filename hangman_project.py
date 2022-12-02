b0f6341343f76739095795fcdd92a26661d36d29
Brandon Tye <brandotie@gmail.com>
2022-12-02 20:48:31 +0000

    experimental branch with gameContext paradigm

--- hangman_project.py
+++ hangman_project.py
@@ -1,4 +1,12 @@
-print('''\nHello player. Welcome to 
+wrong=0
+word="pretty"
+stages=['','_____','|  |','|  O','| /|\\','|  |','| / \\']
+letters=word
+board=list('_'*len(word))
+evalBoard = "".join(board)
+
+def startScreen():
+    print('''\nHello player. Welcome to 
 
          H   H      A      N     N  G G G G  M         M      A      N     N
          H   H     A A     N N   N  G        M M     M M     A A     N N   N
@@ -6,78 +14,90 @@ print('''\nHello player. Welcome to
          H   H   A A A A   N   N N  G     G  M   M M   M   A A A A   N   N N
          H   H  A       A  N     N  G G G G  M    M    M  A       A  N     N 
 
-         Your life is at stake, so beware :)
-         Below is the word you have to guess.''')
+Your life is at stake, so beware :)
+Below is the word you have to guess.''')
+    print(evalBoard)
+
+def updateBoard():
+    return "".join(board)
+
+def getInputCharacter():
+    '''
+        Get Input character asks user to input their 'guess' into terminal,
+        Then we check for errors: if errors -> print ErrorMessage and return None
+        If no errors -> call evalInputCharacter and progress with game
+    '''
+    print('\n'+'Guess a letter (if you dare).')
+    character=input('Your guess: ').lower()
+
+    #Checks for character length
+    if len(character) != 1:
+        printErrorMessage('Type only one letter you choom.', evalBoard)
+        return
+
+    #checks if character not in [A-Z]
+    if(not character.isalpha()):
+        printErrorMessage('Type only alphabetic letters you choom.', evalBoard)
+        return
+    
+    return { "word": word, "wrong": wrong, "board": board, "evalBoard": evalBoard, "stages": stages, "letters": letters}
+
+def evalInputCharacter(character, gameContext):
+    '''
+        Take character (argument) and then checks if character is in word.
+        If in word -> update temp_board
+    '''
+    if character in gameContext["letters"]:
+        for i in range(len(gameContext["letters"])):
+            if character==gameContext[letters][i]:
+                board[i]=character
+                evalBoard = updateBoard()
+            else:
+                continue
+        # updated_board=''.join(temp_board)
+        letters=word.replace(character,'$')
+        print('\n'+'Good guess g. Keep it going'+'\n')
+        print('Board: {}'.format(evalBoard))
+        
+
+    elif character not in letters:
+        wrong+=1
+        print('\nBad guess. You\'re one step closer to dying cous')
+        print('\n',board,'              ','Wrong guesses:',wrong)
+        print(''.join(stages[:wrong]))
+
+    return evalBoard, wrong
+
+def evalEndCondition(evalBoard, wrong):
+    '''
+        check against wrong and win
+        if wrong -> run = false; 
+    '''
+    print(wrong)
+    if( wrong > len(stages)):
+        print('\nThe word was {word}.\nDoes not matter tho, cause you already dead.\nGood luck in the afterlife.'.format(word=word.upper()))
+        return True
+    
+    if(evalBoard == word):
+        print('\nThe word was {word}.\nCongratulations on guessing it right choom, you may now leave alive.'.format(word=word.upper()))
+        return True
+    
+    return False
 
 def printErrorMessage(msg, temp_board):
     print(msg)
     print('Board:'+ ''.join(temp_board))
 
 def hangman(word):
-    wrong=0
-    stages=['','_____','|  |','|  O','| /|\\','|  |','| / \\']
-    letters=word
-    # letters=list(word)
-    board='_'*len(word)
-    temp_board=list('_'*len(word))
-    print (board)
-    win=0
-    while wrong < len(stages) and '_' in temp_board:
-        print('\n'+'Guess a letter (if you dare).')
-        character=input('Your guess: ').lower()
-
-        #Checks for character length
-        if len(character) != 1:
-            printErrorMessage('Type only one letter you choom.', temp_board)
-            continue
-
-        #checks if character not in [A-Z]
-        if(not character.isalpha()):
-            printErrorMessage('Type only alphabetic letters you choom.', temp_board)
-            continue
-
-        if character in letters:
-            for i in range(len(letters)):
-                if character==letters[i]:
-                    temp_board[i]=character
-                else:
-                    continue
-            # updated_board=''.join(temp_board)
-            letters=word.replace(character,'$')
-            print('\n'+'Good guess g. Keep it going'+'\n')
-            print('Board:'+ ''.join(temp_board))
-            win+=1
-
-        elif character not in letters:
-            wrong+=1
-            print('\nBad guess. You\'re one step closer to dying cous')
-            print(board)
-            print('\n'.join(stages[:wrong]))
-
-    if win > 0:
-        print('\nThe word was {word}.\nCongratulations on guessing it right choom, you may now leave alive.'.format(word=word.upper()))
-
-    else:
-        print('\nThe word was {word}.\nDoes not matter tho, cause you already dead.\nGood luck in the afterlife.'.format(word=word.upper()))
-
-        
-        
-
-            
-
+    startScreen()
     
+    while True:
+        gameContext = getInputCharacter()
+        gameContext = evalInputCharacter(gameContext)
+        evalEnd = evalEndCondition(evalBoard, wrong)
+
+        if(evalEnd):
+            break
 
 
-
-
-print(hangman('pretty'))
-#     for stage in stages:
-#         print(stage)
-
-# print(hangman())
-
-
-
-
-
-   
+hangman(word)
